da497d2fa8aa58e069609d87bdbf72b0ae49e6df
Hakan Demir <hhkandemir@gmail.com>
2025-11-27 15:34:08 +0300

    Add string manipulation examples in strings.py

--- strings.py
+++ strings.py
@@ -28,3 +28,36 @@ result = course[-15:]  # stringlerde sondan başlarsak -1 -2 ... diye sayarız. 
 #5 'course' ifadesindeki karakterleri tersten yazdırın.
 
 result = course[::-1]
+
+nama, surname, age, job = 'Bora', 'Yılmaz', 32, 'mühendis'
+
+#6 Yukarıdaki verilen değişkenler ile ekrana aşağıdaki ifadeyi yazdırın. 
+#  "Benim adım Bora Yılmaz, Yaşım 32 ve Mesleğim mühendis"
+
+result = "Benim adım " + name + " " + surname + " Yaşım " + str(age) + " ve Mesleğim " + job
+result = 'Benim adım {0} {1}, Yaşım {2} ve Mesleğim {3}'.format(name,surname,age,job)
+result = f"Benim adım {name} {surname}, Yaşım {age} ve Mesleğim {job}"
+
+#7 'Hello world' ifadesindeki w karfini W ile değiştirin.
+
+s = 'Hello world'
+result = s[0:6] + 'W' + s[-4:]
+
+#8 'abc ifadesini yan yana 3 defa yazdırın'
+
+result = 'abc' * 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
